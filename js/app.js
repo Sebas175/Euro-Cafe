81cdfef7fcc6628b44ae9925c805b5f9aae78a2e
artavia17 <artaviaalonso60@gmail.com>
2022-03-11 13:05:10 -0600

    Agregando info

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 'user strict'
 
 //Nota: Aqui solo van importaciones de codigo, no colocar codigo global aqui para no hacer desorden.
+import saludo from "./mostar_paginas.js";
+
+saludo();
---
+++ js/mostar_paginas.js
@@ -0,0 +1,15 @@
+
+
+const cargar_paginas = () =>{
+
+    const index = document.querySelector("#cuerpo");
+    const inicio = document.querySelector("#inicio")
+    window.onload = () =>{
+
+        console.log(inicio)
+
+    }
+
+} 
+
+export default cargar_paginas;
